d7283cfb9caf2e756f8a593436b8a45af47f5cf8
Rajendran <ragulkumar.rajendran@accenture.com>
2018-01-19 17:53:08 +0100

    The video is playing on the browser. The files are sorted while writing to BigFrame.jpeg

--- Punahele/src/com/punahele/utilities/video/VideoToFramesConverter.java
+++ Punahele/src/com/punahele/utilities/video/VideoToFramesConverter.java
@@ -9,6 +9,12 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
 import java.nio.channels.WritableByteChannel;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
 
 import javax.imageio.ImageIO;
 
@@ -32,7 +38,7 @@ public class VideoToFramesConverter {
 			AWTFrameGrab fg = AWTFrameGrab.createAWTFrameGrab(in);
 			String parts[] = fileName.split("\\.");
 			new File(parts[0]).mkdir();
-			for (int i = 0; i < 1000; i++) {
+			for (int i = 0; i < 2000; i++) {
 				BufferedImage frame = fg.getFrame();
 				File file = new File(parts[0]+"\\Frame" + i + ".jpeg");
 				file.createNewFile();
@@ -47,7 +53,19 @@ public class VideoToFramesConverter {
 	public void mergeImagesToFile(String folderName, String finalFilename) throws IOException {
 		StringBuilder positions = new StringBuilder();
 		File videoFolder = new File(folderName);
-		File[] frames = videoFolder.listFiles();
+		//File[] frames = videoFolder.listFiles();
+		
+		List<File> frames = Arrays.asList(videoFolder.listFiles());
+		Collections.sort(frames, new Comparator<File>() {
+			@Override
+			public int compare(File f1, File f2) {
+				int num1 = Integer.parseInt(f1.getName().substring(5, f1.getName().indexOf(".")));
+				int num2 = Integer.parseInt(f2.getName().substring(5, f2.getName().indexOf(".")));
+				return num1 - num2;
+			}
+		});
+		
+		
 		final Closer closer = Closer.create();
 
 		//Sort the frames and then merge..VVI
@@ -91,5 +109,22 @@ public class VideoToFramesConverter {
 	public static void main(String[] args) throws IOException, JCodecException {
 		new VideoToFramesConverter().convertVideoToFrame("C:\\Videos\\OneRepublic - Feel Again.mp4");
 		//new VideoToFramesConverter().mergeImagesToFile("C:\\Videos\\OneRepublic - Feel Again\\Gen\\TextMerge", "C:\\Videos\\OneRepublic - Feel Again\\Gen\\TextMerge\\fin.txt");
+		File videoFolder = new File("C:\\Videos\\OneRepublic - Feel Again");
+		List<File> frames = Arrays.asList(videoFolder.listFiles());
+		Collections.sort(frames, new Comparator<File>() {
+			@Override
+			public int compare(File o1, File o2) {
+				int num1 = Integer.parseInt(o1.getName().substring(5, o1.getName().indexOf(".")));
+				int num2 = Integer.parseInt(o2.getName().substring(5, o2.getName().indexOf(".")));
+				return num1 - num2;
+			}
+		});
+		frames.forEach(new Consumer<File>() {
+
+			@Override
+			public void accept(File t) {
+				System.out.println(t.getName());
+			}
+		});
 	}
 }
